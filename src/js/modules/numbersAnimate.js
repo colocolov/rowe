77b0f23b9df23f9b6530eb87f22e9905e6e1b781
colocolov <colocolov.stas@gmail.com>
2025-02-28 13:55:48 +0200

    updates

--- src/js/modules/numbersAnimate.js
+++ src/js/modules/numbersAnimate.js
@@ -33,7 +33,7 @@ document.addEventListener("DOMContentLoaded", () => {
     // Проверяем, видна ли секция на 80%
     if (!hasAnimated && sectionTop < screenHeight * 0.95 && sectionTop + sectionHeight > 0) {
       hasAnimated = true; // Устанавливаем флаг, чтобы не запускать анимацию повторно
-      console.log(numbersVideo);
+      // console.log(numbersVideo);
       animateNumbers(); // Запускаем анимацию чисел
       numbersVideo.load();
       numbersVideo.play();
